7f49049643f4e147790511070fc99f5deb64d7bd
Paulo Rocha <paulorocha1988@gmail.com>
2020-06-18 14:42:16 +0100

    deck starts empty

---
+++ src/main/java/Card.java
@@ -0,0 +1,9 @@
+public enum Card {
+//    private SuitType suit;
+//    private CardRank rank;
+
+//    public Card(SuitType suit, CardRank rank){
+//        this.suit = suit;
+//        this.rank = rank;
+//    }
+}
